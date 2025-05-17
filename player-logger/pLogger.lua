4b1163fd42449cc415bd8b9cccbad1c716702dc9
ant.tonchev <ant.tonchev@gmail.com>
2025-05-17 13:43:05 +0300

    Player logger improvements

--- player-logger/pLogger.lua
+++ player-logger/pLogger.lua
@@ -1,25 +1,25 @@
-Logger = {
+logger = {
     logFile = nil,
 
-    init = function(self, filename)
-        self.logFile = filename or "log.txt"
+    init = function(filename)
+        logger.logFile = filename or "log.txt"
     end,
 
     formatTime = function()
-        return textutils.formatTime(os.time(), true)
+        return os.date("%Y-%m-%d %H:%M:%S")
     end,
 
-    log = function(self, message)
-        if not self.logFile then
+    log = function(logger, message)
+        if not logger.logFile then
             error("Logger not initialized. Call init() first.")
         end
 
-        local file = fs.open(self.logFile, "a")
+        local file = fs.open(logger.logFile, "a")
         if not file then
-            error("Could not open log file: " .. self.logFile)
+            error("Could not open log file: " .. logger.logFile)
         end
 
-        local timeStr = self:formatTime()
+        local timeStr = logger.formatTime()
         local logEntry = string.format("[%s] %s\n", timeStr, message)
 
         file.write(logEntry)
@@ -33,10 +33,10 @@ while true do
     local event, username, dimensions = os.pullEvent("playerJoin")
     if event then
         chatbox.sendMessage("Hello There, " .. username)
-        chatbox.sendMessageToPlayer("How's the " .. dimensions.."?")
-        logger.log(logger, username .. " Joined.")
+        chatbox.sendMessageToPlayer("How's the " .. dimensions.."?", username)
+        logger.log( username .. " Joined.")
         if "srooku" == string.lower(username) then
-            chatbox.sendMessageToPlayer("Don't forget to manage your tutels.")
+            chatbox.sendMessageToPlayer("Don't forget to manage your tutels.", username)
         end
     end
 end
